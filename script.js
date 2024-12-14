61f9409bf87d45c6bb3fde56922e398aa5fdbe66
Harsh  Kumar Maurya <Harsh.lko2004@gmail.com>
2024-12-14 16:07:45 +0530

    i made changes

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ function navAnimation() {
 nav.addEventListener("mouseenter",function(){
   let tl=gsap.timeline()
     tl.to("#nav-bottom",{
-        height:"22vh"
+        height:"22vh",
+        duration: 0.5
     })
     tl.to(".nav-part2 h5",{
-        display:"block"
+        display:"block",
+        duration: 0.1
     })
     tl.to(".nav-part2 h5 span",{
        y:0,
@@ -36,4 +38,72 @@ nav.addEventListener("mouseleave",function(){
       })
   })
 }
-navAnimation()
+// navAnimation()
+function page2Animation() {
+  var rightElems = document.querySelectorAll(".right-elem")
+
+  rightElems.forEach(function (elem) {
+      elem.addEventListener("mouseenter", function () {
+
+
+
+
+          gsap.to(elem.childNodes[3], {
+              opacity: 1,
+              scale: 1
+          })
+      })
+      elem.addEventListener("mouseleave", function () {
+          gsap.to(elem.childNodes[3], {
+              opacity: 0,
+              scale: 0
+          })
+      })
+      elem.addEventListener("mousemove", function (dets) {
+
+          gsap.to(elem.childNodes[3], {
+              x: dets.x - elem.getBoundingClientRect().x - 90,
+              y: dets.y - elem.getBoundingClientRect().y - 215
+          })
+      })
+  })
+}
+page2Animation()
+
+function page3VideoAnimation() {
+  var page3Center = document.querySelector(".page3-center")
+  var video = document.querySelector("#page3 video")
+
+  page3Center.addEventListener("click", function () {
+      video.play()
+      gsap.to(video, {
+          transform: "scaleX(1) scaleY(1)",
+          opacity: 1,
+          borderRadius: 0
+      })
+  })
+  video.addEventListener("click", function () {
+      video.pause()
+      gsap.to(video, {
+          transform: "scaleX(0.7) scaleY(0)",
+          opacity: 0,
+          borderRadius: "30px"
+      })
+  })
+
+
+  var sections = document.querySelectorAll(".sec-right")
+
+  sections.forEach(function (elem) {
+      elem.addEventListener("mouseenter", function () {
+          elem.childNodes[3].style.opacity = 1
+          elem.childNodes[3].play()
+      })
+      elem.addEventListener("mouseleave", function () {
+          elem.childNodes[3].style.opacity = 0
+          elem.childNodes[3].load()
+      })
+  })
+
+}
+page3VideoAnimation()
